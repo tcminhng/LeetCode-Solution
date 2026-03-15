766dc3ab37fc649f84ffa06d335abc61d9f875a3
Minh Nguyen <tcminh.ng20@gmail.com>
2026-03-15 12:20:04 -0700

    Create Products of Array Except Self-redo4.java

---
+++ Arrays & Hashing/Products of Array Except Self-redo4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] answer = new int[len];
+
+        answer[0] = 1;
+        for(int i = 1; i < len; i++){
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+
+        int right = 1;
+        for(int i = len - 1; i >= 0; i--){
+            answer[i] = right * answer[i];
+            right = right * nums[i];
+        }
+
+        return answer;
+    }
+}  
